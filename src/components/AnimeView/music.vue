d7fd1a10536a5299197e2c46109ca5af966e625d
Everton Vargas Guetierres <evertoniee@yahoo.com.br>
2022-11-23 18:03:58 -0300

    ajuste bug videos

--- src/components/AnimeView/music.vue
+++ src/components/AnimeView/music.vue
@@ -2,7 +2,7 @@
     <div class="fixed top-0 left-0 z-50 w-screen h-screen bg-black/70 flex items-center justify-center">
         <div class="flex flex-col max-w-[90%] w-[960px] gap-5" @click.stop> 
             <select v-model="video" class="w-full p-4 cursor-pointer outline-none text-graytext">
-                <option value="" selected disabled>Por favor, selecione uma opening ou ending para visualizar</option>
+                <option selected disabled>Por favor, selecione uma opening ou ending para visualizar</option>
                 <option v-for="(video,index) in videos" :key="index" class="cursor-pointer hover:bg-star" :value="video.link">{{video.name}}</option>
                 
             </select>
@@ -18,5 +18,10 @@
 export default {
     props:['videos'],
     name:'musicFrame',
+    data(){
+        return{
+            video:''
+        }
+    }
 }
 </script>
